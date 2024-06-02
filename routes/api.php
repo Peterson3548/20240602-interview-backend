4468b7a86224074d0e2821d51867f5af47f637ac
gary <a27487770@gmail.com>
2024-06-02 09:07:35 +0000

    fix api.php: add prefix "api" for route name

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::name('api.')->group(function () {
+    Route::apiResource('brands', BrandController::class)->only('index');
+
+    Route::apiResource('products', ProductController::class)->only('index');
 });
-
-Route::apiResource('brands', BrandController::class)->only('index');
-
-Route::apiResource('products', ProductController::class)->only('index');
